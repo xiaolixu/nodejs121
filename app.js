27e4b21caed2ea54d4a9d06fbe70b46c3b506920
xiaolixu <xiaolixu@cn.ibm.com>
2017-03-07 11:10:40 +0800

    init nlu

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-/*eslint-env node*/
+'use strict';
+const fs = require('fs');
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+require('dotenv').config({ silent: true }); //  optional
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+const nlu = new NaturalLanguageUnderstandingV1({
+  // note: if unspecified here, credentials are pulled from environment properties:
+  // NATURAL_LANGUAGE_UNDERSTANDING_USERNAME &  NATURAL_LANGUAGE_UNDERSTANDING_PASSWORD
+   username: '6556a737-8135-4f01-a0f0-a177b6dca4d1',
+   password: 'zlPWIsGMEjDJ',
+  version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2016_01_23
+});
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+const filename = 'energy-policy.html';
+fs.readFile(filename, 'utf-8', function(file_error, file_data) {
+  if (file_error) {
+    console.log(file_error);
+  } else {
+    const options = {
+      html: file_data,
+      features: {
+        concepts: {},
+        keywords: {}
+      }
+    };
+    nlu.analyze(options, function(err, res) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(res);
+    });
+  }
 });
